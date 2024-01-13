c615969ca03b2f03acf9fe60cad3b30c40787e69
zxcej <44418688+zxcej@users.noreply.github.com>
2024-01-14 06:35:34 +0900

    Update unetformer.py

--- open_earth_map/networks/unetformer.py
+++ open_earth_map/networks/unetformer.py
@@ -476,7 +476,7 @@ class UNetFormer(nn.Module):
             backbone_name,
             features_only=True,
             output_stride=32,
-            out_indices=(1, 2, 3, 4),
+            out_indices=(0, 1, 2, 3),
             pretrained=pretrained,
             in_chans=in_channels,
         )
